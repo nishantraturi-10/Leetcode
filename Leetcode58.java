e83bb55d237fcee3dc96a92284fb6a30ccbd07b8
mehere10 <nishantraturi10@gmail.com>
2024-09-10 20:50:40 +0530

    updated

--- Leetcode58.java
+++ Leetcode58.java
@@ -1,4 +1,18 @@
 package LeetcodeProblems;
 
 public class Leetcode58 {
+    public static int lengthOfLastWord(String s) {
+        int count=0;
+        String str=s.trim();
+        int index=str.length()-1;
+        while(index>=0 && str.charAt(index)!=' '){
+            count++;
+            index--;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+     String str=" Hello World ";
+     System.out.println("The length of last wrod is : "+lengthOfLastWord(str));
+    }
 }
