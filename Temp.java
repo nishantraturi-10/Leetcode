e8134218a4a8d0cedb202ba3fbae3d7ac0ff9a2a
mehere10 <nishantraturi10@gmail.com>
2024-09-18 21:38:30 +0530

    updated

--- Temp.java
+++ Temp.java
@@ -1,8 +1,30 @@
 package LeetcodeProblems;
 
 public class Temp {
-    public static void main(String[] args) {
 
-        StringBuilder sb=new StringBuilder("");
+    static int lengthOfLongestSubstring(String s) {
+        int count=1;
+        String ans=""+s.charAt(0);
+        //StringBuilder temp=new StringBuilder("");       // a
+
+        for(int i=1;i<s.length();i++){
+            char current=s.charAt(i);
+            for(int j=0;j<ans.length();j++){
+                if(current==ans.charAt(j)){
+
+                    break;
+                }
+                else{
+                    ans+=current;
+                    count++;
+                }
+            }
+        }
+        System.out.println(ans);
+        return count;
+    }
+    public static void main(String[] args) {
+        String str="abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
     }
 }
